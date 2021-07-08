edf33eb92bdc8325bc12ba5313eba391436c0e34
djbarcelos <djbarcelos@gmail.com>
2021-07-07 21:34:10 -0300

    update release 1.0

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ vsCode() {
   sudo apt install software-properties-common apt-transport-https wget
   wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add --
   sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
-  sudo apt install code
+  sudo apt install code -y
   message
 }
 
@@ -57,7 +57,7 @@ compass() {
 postman() {
     # install Postman
     NAME_INSTALL="Postman"
-  sudo snap install postman
+  sudo snap install postman -y
   message
 }
 
